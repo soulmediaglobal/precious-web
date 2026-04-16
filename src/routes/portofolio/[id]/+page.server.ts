ccd78eafbb658635577ec949716f5e81168af992
fitrul31 <fitrul.islam@gmail.com>
2026-04-16 11:06:33 +0700

    chore: remove console

--- src/routes/portofolio/[id]/+page.server.ts
+++ src/routes/portofolio/[id]/+page.server.ts
@@ -3,9 +3,7 @@ import { portofolio } from '$lib';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = ({ params }) => {
-  console.log('params', params);
   const item = portofolio.find((p) => p.slug === params.id);
-  console.log('item', item);
 
   if (!item) {
     throw error(404, 'Portofolio not found');
